5df17f31cee6e730b40f3d7faf0086918226b96c
Ubuntu <ubuntu@ip-172-31-43-197.ap-south-1.compute.internal>
2023-08-01 13:20:16 +0000

    jenkins deploy code

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $GIT_BRANCH == dev ]]; then
-    cd react
+    cd Capstone-project
     chmod +x build.sh
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
     echo "$docker_password" | docker login -u "$docker_username" --password-stdin
     docker image tag react:v1 "$docker_username/dev:v1"
     docker image push "$docker_username/dev:v1"
-elif [[ $GIT_BRANCH == master ]]; then
-    cd react
+elif [[ $GIT_BRANCH == main ]]; then
+    cd Capstone-project
     chmod +x build.sh
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
@@ -19,16 +19,3 @@ elif [[ $GIT_BRANCH == master ]]; then
 else
     echo "Deployment is failure"
 fi
-
- # Make the shell script executable
-  chmod +x build.sh
-
-  # Build and run the Docker containers using Docker Compose.
-  ./build.sh
-
-  # Optional: Stop and remove any previously running containers to clean up
-  docker-compose down
-
-  # Start the Docker containers in the background
-  docker-compose up -d
-EOF
