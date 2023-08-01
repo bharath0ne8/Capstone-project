fcbea0fa85abc22f4a8726941ccd49174fafb321
Ubuntu <ubuntu@ip-172-31-43-197.ap-south-1.compute.internal>
2023-08-01 05:42:25 +0000

    Add new feature to the dev branch

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-# Public IP address of your EC2 instance
-EC2_PUBLIC_IP="13.233.164.216"
+if [[ $GIT_BRANCH == dev ]]; then
+    cd react
+    chmod +x build.sh
+    ./build.sh
+    docker login --username="$docker_username" --password="$docker_password"
+    echo "$docker_password" | docker login -u "$docker_username" --password-stdin
+    docker image tag react:v1 "$docker_username/dev:v1"
+    docker image push "$docker_username/dev:v1"
+elif [[ $GIT_BRANCH == master ]]; then
+    cd react
+    chmod +x build.sh
+    ./build.sh
+    docker login --username="$docker_username" --password="$docker_password"
+    echo "$docker_password" | docker login -u "$docker_username" --password-stdin
+    docker image tag react:v1 "$docker_username/prod:v1"
+    docker image push "$docker_username/prod:v1"
+else
+    echo "Deployment is failure"
+fi
 
-# SSH key to access the EC2 instance. Replace "YOUR_SSH_KEY.pem" with your actual SSH key file.
-SSH_KEY="capstone.pem"
-
-# SSH user for the EC2 instance. Replace "ec2-user" with your EC2 instance's username.
-SSH_USER="ubuntu"
-
-# Name of the Docker Compose YAML file (assuming it's in the same directory as the script).
-DOCKER_DOCKERFILE="Dockerfile"
-
-
-# Name of the Docker Compose YAML file (assuming it's in the same directory as the script).
-DOCKER_COMPOSE_FILE="docker-compose.yml"
-
-# Copy the Docker Compose YAML file and the shell script to the EC2 instance.
-scp -i "$SSH_KEY" "$DOCKER_DOCKERFILE" "$SSH_USER"@"$EC2_PUBLIC_IP":/home/"$SSH_USER"/
-scp -i "$SSH_KEY" "$DOCKER_COMPOSE_FILE" "$SSH_USER"@"$EC2_PUBLIC_IP":/home/"$SSH_USER"/
-scp -i "$SSH_KEY" build.sh "$SSH_USER"@"$EC2_PUBLIC_IP":/home/"$SSH_USER"/
-
-# SSH into the EC2 instance and deploy the Docker containers.
-ssh -i "$SSH_KEY" "$SSH_USER"@"$EC2_PUBLIC_IP" << EOF
-  # Go to the directory where the Docker Compose file is located
-  cd /home/"$SSH_USER"
-
-  # Make the shell script executable
+ # Make the shell script executable
   chmod +x build.sh
 
   # Build and run the Docker containers using Docker Compose
