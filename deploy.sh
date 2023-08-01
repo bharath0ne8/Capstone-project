f1fc24980e990c0e8b2aac823afff29e05db172d
Ubuntu <ubuntu@ip-172-31-43-197.ap-south-1.compute.internal>
2023-08-01 14:01:09 +0000

    deploy  code

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [[ $GIT_BRANCH == dev ]]; then
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
     echo "$docker_password" | docker login -u "$docker_username" --password-stdin
-    docker image tag react:v1 "$docker_username/dev:v1"
+    docker image tag capstone-app-image:v1 "$docker_username/dev:v1"
     docker image push "$docker_username/dev:v1"
 elif [[ $GIT_BRANCH == main ]]; then
     cd Capstone-project
@@ -14,7 +14,7 @@ elif [[ $GIT_BRANCH == main ]]; then
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
     echo "$docker_password" | docker login -u "$docker_username" --password-stdin
-    docker image tag react:v1 "$docker_username/prod:v1"
+    docker image tag capstone-app-image:v1 "$docker_username/prod:v1"
     docker image push "$docker_username/prod:v1"
 else
     echo "Deployment is failure"
