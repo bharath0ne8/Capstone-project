8991417fd3dffbdbf37a62e33c7c90f5cf2ffe67
Ubuntu <ubuntu@ip-172-31-43-197.ap-south-1.compute.internal>
2023-08-01 13:00:49 +0000

    jenkins code

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $GIT_BRANCH == dev ]]; then
-    cd react
+    cd Capstone-project
     chmod +x build.sh
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
     echo "$docker_password" | docker login -u "$docker_username" --password-stdin
     docker image tag react:v1 "$docker_username/dev:v1"
     docker image push "$docker_username/dev:v1"
-elif [[ $GIT_BRANCH == master ]]; then
-    cd react
+elif [[ $GIT_BRANCH == main ]]; then
+    cd Capstone-project
     chmod +x build.sh
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
