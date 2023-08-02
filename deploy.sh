9108f1a4d3dfcc3f0a775222d472d6eaf2556122
Ubuntu <ubuntu@ip-172-31-43-197.ap-south-1.compute.internal>
2023-08-02 06:43:50 +0000

    jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ docker_username: 'bharathsrinivasan'
 docker_password: 'dckr_pat_wT2df4Kw2z-5bLCvtcHYOcENHHo'
 
 if [[ $GIT_BRANCH == dev ]]; then
-    cd Capstone-project
     chmod +x build.sh
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
@@ -12,7 +11,6 @@ if [[ $GIT_BRANCH == dev ]]; then
     docker tag capstone-app-image:v1 "$docker_username/dev:v1"
     docker push "$docker_username/dev:v1"
 elif [[ $GIT_BRANCH == main ]]; then
-    cd Capstone-project
     chmod +x build.sh
     ./build.sh
     docker login --username="$docker_username" --password="$docker_password"
